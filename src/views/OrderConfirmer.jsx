215674d4da836096ce9a69aa4a52480629f4b7e1
KikoRiquel <1696627@alu.murciaeduca.es>
2024-06-18 21:48:28 +0200

    faltaba el logo 👅

--- src/views/OrderConfirmer.jsx
+++ src/views/OrderConfirmer.jsx
@@ -1,3 +1,4 @@
+import { Image } from "@nextui-org/react";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -13,7 +14,14 @@ export const OrderConfirmer = () => {
   }, [navigate]);
 
   return (
-    <div className="bg-orange-500 font-bold text-3xl text-white flex items-center justify-center h-screen">
+    <div className="bg-orange-500 font-bold text-3xl text-white flex flex-col items-center h-dvh">
+      <div className="flex justify-center">
+          <Image
+            alt="logo"
+            width={200}
+            src={`..\\src\\assets\\img\\logo.png`}
+          />
+        </div>
       <h1>¡Pedido confirmado!</h1>
     </div>
   );
